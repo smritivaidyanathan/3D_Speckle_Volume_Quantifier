31453da1fbc439bec91132870a31e8611553b28d
Smriti Vaidyanathan <smriti_vaidyanathan@brown.edu>
2023-11-14 15:27:38 -0500

    Added status messages

--- image_processor.py
+++ image_processor.py
@@ -14,16 +14,27 @@ import os
 
 def nd2converter(nd2_file_path, channel_num):
     nd2 = nd2reader.ND2Reader(nd2_file_path)
-    print(nd2.sizes['z'])
+    print(f"{nd2.sizes['z']} z-stacks in {nd2_file_path} found.")
     arr = []
+    print("Adding z-stacks...")
     for z in range (nd2.sizes['z']):
         arr.append(nd2.get_frame_2D(channel_num, 0, z, 0, 0, 0))
+        progress = round(float(z/nd2.sizes['z']) * 100)
+        print(f"{round(float(z/nd2.sizes['z']) * 100)}% done.")
     arr = np.array(arr, dtype='uint16')
+    print("Done adding z-stacks.")
+
+    z, h, w = np.shape(arr)
+    if (z == nd2.sizes['z']):
+        print(f"Success! Tiff with {z} z-stacks of {w}x{h} size images was created")
+    else:
+        print(f"Uh oh! Something went wrong. Tiff with {z} z-stacks of {w}x{h} size images was created. Is this what you expected?")
+        return
     io.imsave(f'tiff_images/{os.path.basename(nd2_file_path)}_C-{channel_num}.tiff', arr)
     return (f'tiff_images/{os.path.basename(nd2_file_path)}_C-{channel_num}.tiff')
 
 
-def imageProcessor(path):
+def imageProcessor(path, speckle_volumes):
     # Load your 3D stack image
     image = np.stack(io.imread(path), axis = 0)
 
@@ -39,11 +50,9 @@ def imageProcessor(path):
 
     # 3D Region Segmentation _ just using connectivity, like a fill bucket tool!
     labeled_image = measure.label(binary_image3, connectivity=None)  # Adjust the connectivity as needed
-    print(labeled_image)
 
     # Volume Calculation
     regions = measure.regionprops(labeled_image)
-    speckle_volumes = []
     for region in regions:
         volume = region.area  # In voxels
         speckle_volumes.append(volume)
@@ -81,8 +90,8 @@ def viewSegmentation (path):
     viewer.show()
 
     # Volume Calculation
-    regions = measure.regionprops(labeled_image)
     speckle_volumes = []
+    regions = measure.regionprops(labeled_image)
     for region in regions:
         volume = region.area  # In voxels
         speckle_volumes.append(volume)
@@ -91,7 +100,7 @@ def viewSegmentation (path):
     plt.hist(speckle_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400))
     plt.xlabel('Speckle Volume (pixels)')
     plt.ylabel('Frequency (number)')
-    plt.title('Distribution of Speckle Volumes')
+    plt.title(f"Distribution of Speckle Volumes N = {len(speckle_volumes)}")
     plt.grid(True)
 
 
@@ -102,8 +111,31 @@ def viewSegmentation (path):
 
 
 
-def loopThroughAllImages(path_to_nd2, channel_num):
-    viewSegmentation(nd2converter(path_to_nd2, channel_num))
+def loopThroughAllImages(path_to_nd2, nd2_image_names, channel_num):
+    speckle_volumes = []
+    for nd2 in nd2_image_names:
+        tiff_path = f"tiff_images/{os.path.basename(path_to_nd2 + nd2)}_C-{channel_num}.tiff"
+        if os.path.exists(tiff_path):
+            numvol = len(speckle_volumes)
+            print("\nTiff file " +tiff_path + " already exists.")
+            speckle_volumes = imageProcessor(tiff_path, speckle_volumes)
+            print("Identified " + str(len(speckle_volumes) - numvol) + " volumes from " + nd2)
+        else:
+            numvol = len(speckle_volumes)
+            print("\nTiff file " + tiff_path + " does not exist yet. Converting from nd2.") 
+            speckle_volumes = imageProcessor(nd2converter(path_to_nd2 + nd2, channel_num), speckle_volumes)
+            print("Identified " + str(len(speckle_volumes) - numvol) + " volumes from " + nd2)
+    # Create a histogram of speckle volumes
+    print("\nDone. Creating Distribution Histogram.")
+    plt.hist(speckle_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400))
+    plt.xlabel('Speckle Volume (pixels)')
+    plt.ylabel('Frequency (number)')
+    plt.title(f"Distribution of Speckle Volumes (N={len(speckle_volumes)})")
+    plt.grid(True)
 
-path_to_folder = "/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)" + "/Hrp38GFP_CLAMP(red)_con001.nd2"
-loopThroughAllImages(path_to_folder, 0)
+
+    # Show the plot
+    plt.show()
+
+images = ["Hrp38GFP_CLAMP(red)_con001.nd2", "Hrp38GFP_CLAMP(red)_con002.nd2", "Hrp38GFP_CLAMP(red)_con003.nd2", "Hrp38GFP_CLAMP(red)_con004.nd2", "Hrp38GFP_CLAMP(red)_con005.nd2", "Hrp38GFP_CLAMP(red)_con006.nd2", "Hrp38GFP_CLAMP(red)_con007.nd2"]
+loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", images,  0)
