cbc7d1613fd540da5da1edffd7b0108007bbe1f9
Smriti Vaidyanathan <smriti_vaidyanathan@brown.edu>
2023-11-15 15:29:27 -0500

    Added run-through + Additional histograms.

--- image_processor.py
+++ image_processor.py
@@ -25,11 +25,11 @@ def nd2converter(nd2_file_path, channel_num):
     print("Done adding z-stacks.")
 
     z, h, w = np.shape(arr)
-    if (z == nd2.sizes['z']):
+    if (z == nd2.sizes['z'] and nd2.sizes['c'] == 3):
         print(f"Success! Tiff with {z} z-stacks of {w}x{h} size images was created")
     else:
         print(f"Uh oh! Something went wrong. Tiff with {z} z-stacks of {w}x{h} size images was created. Is this what you expected?")
-        return
+        return -1
     io.imsave(f'tiff_images/{os.path.basename(nd2_file_path)}_C-{channel_num}.tiff', arr)
     return (f'tiff_images/{os.path.basename(nd2_file_path)}_C-{channel_num}.tiff')
 
@@ -44,7 +44,12 @@ def imageProcessor(path, speckle_volumes):
 
 
     # Thresholding
-    binary_image3 = smoothed_image > 632
+    print(np.mean(smoothed_image))
+    print(np.std(smoothed_image))
+    threshold_new = np.mean(smoothed_image) + (8*np.std(smoothed_image))
+    print(threshold_new)
+    # Thresholding
+    binary_image3 = smoothed_image > threshold_new
 
 
 
@@ -65,9 +70,12 @@ def viewSegmentation (path):
     sigma = 1.0
     smoothed_image = gaussian_filter(image, sigma=sigma)
 
-
+    print(np.mean(smoothed_image))
+    print(np.std(smoothed_image))
+    threshold_new = np.mean(smoothed_image) + (8*np.std(smoothed_image))
+    print(threshold_new)
     # Thresholding
-    binary_image3 = smoothed_image > 632
+    binary_image3 = smoothed_image > threshold_new
 
 
 
@@ -117,25 +125,85 @@ def loopThroughAllImages(path_to_nd2, nd2_image_names, channel_num):
         tiff_path = f"tiff_images/{os.path.basename(path_to_nd2 + nd2)}_C-{channel_num}.tiff"
         if os.path.exists(tiff_path):
             numvol = len(speckle_volumes)
-            print("\nTiff file " +tiff_path + " already exists.")
+            print("Tiff file " +tiff_path + " already exists.")
             speckle_volumes = imageProcessor(tiff_path, speckle_volumes)
             print("Identified " + str(len(speckle_volumes) - numvol) + " volumes from " + nd2)
         else:
             numvol = len(speckle_volumes)
-            print("\nTiff file " + tiff_path + " does not exist yet. Converting from nd2.") 
-            speckle_volumes = imageProcessor(nd2converter(path_to_nd2 + nd2, channel_num), speckle_volumes)
+            print("Tiff file " + tiff_path + " does not exist yet. Converting from nd2.") 
+            result = nd2converter(path_to_nd2 + nd2, channel_num)
+            if (result == -1):
+                print("Skipping ... Error found ...")
+                continue
+            speckle_volumes = imageProcessor(result, speckle_volumes)
             print("Identified " + str(len(speckle_volumes) - numvol) + " volumes from " + nd2)
-    # Create a histogram of speckle volumes
-    print("\nDone. Creating Distribution Histogram.")
-    plt.hist(speckle_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400))
-    plt.xlabel('Speckle Volume (pixels)')
-    plt.ylabel('Frequency (number)')
-    plt.title(f"Distribution of Speckle Volumes (N={len(speckle_volumes)})")
-    plt.grid(True)
+        print("---------------------------------------------------------")
+    return speckle_volumes
+
+print("\nRunning control images...")
+control_images = ["Hrp38GFP_CLAMP(red)_con001.nd2", "Hrp38GFP_CLAMP(red)_con002.nd2", "Hrp38GFP_CLAMP(red)_con003.nd2", "Hrp38GFP_CLAMP(red)_con004.nd2", "Hrp38GFP_CLAMP(red)_con005.nd2", "Hrp38GFP_CLAMP(red)_con006.nd2", "Hrp38GFP_CLAMP(red)_con007.nd2"]
+control_volumes = loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", control_images,  0)
+print("\n ============================================================================ \n")
+print("\nRunning clamp null images...")
+clamp2null_images = ["Hrp38GFPGFP_CLAMP(red)_Clamp2(null).nd2", "Hrp38GFPGFP_CLAMP(red)_Clamp2(null)001.nd2"]
+clamp2null_volumes = loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", clamp2null_images,  0)
+print("\n ============================================================================ \n")
+print("\nRunning clamp(h) images...")
+clamph_images = ["Hrp38GFPGFP_CLAMP(red)_ClampH.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampH001.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampH002.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampH003.nd2"]
+clamph_volumes = loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", clamph_images,  0)
+print("\n ============================================================================ \n")
+print("\nRunning clamp(i) images...")
+clampi_images = ["Hrp38GFPGFP_CLAMP(red)_ClampI.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampI001.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampI002.nd2", "Hrp38GFPGFP_CLAMP(red)_ClampI003.nd2"]
+clampi_volumes = loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", clampi_images,  0)
+print("\n ============================================================================ \n")
+print("\nAll done!!! :)")
+
+files = os.listdir("tiff_images")
+files = [f for f in files if os.path.isfile(os.path.join("tiff_images", f))]
+print(f"{len(files)}/{len(control_images) + len(clamp2null_images) +  len(clamph_images) + len(clampi_images)} tiff created/used.")
+
+print("Making histograms now...")
+plt.hist(control_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Control")
+plt.hist(clamp2null_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp2 (null)")
+plt.hist(clamph_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp(H)")
+plt.hist(clampi_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp(I)")
+plt.xlabel('Speckle Volume (pixels)')
+plt.ylabel('Probability (number)')
+plt.title(f"Distribution of Probabilities of Speckle Volumes")
+#plt.title(f"Distribution of Speckle Volumes (N={len(speckle_volumes)})")
+plt.grid(True)
+plt.legend()
+plt.show()
 
 
-    # Show the plot
-    plt.show()
+plt.hist(control_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Control")
+plt.xlabel('Speckle Volume (pixels)')
+plt.ylabel('Probability (number)')
+plt.title(f"Distribution of Speckle Volumes (N={len(control_volumes)}) (Control)")
+plt.grid(True)
+plt.legend()
+plt.show()
 
-images = ["Hrp38GFP_CLAMP(red)_con001.nd2", "Hrp38GFP_CLAMP(red)_con002.nd2", "Hrp38GFP_CLAMP(red)_con003.nd2", "Hrp38GFP_CLAMP(red)_con004.nd2", "Hrp38GFP_CLAMP(red)_con005.nd2", "Hrp38GFP_CLAMP(red)_con006.nd2", "Hrp38GFP_CLAMP(red)_con007.nd2"]
-loopThroughAllImages("/volumes/Research/BM_LarschanLab/mukulika/5_13_21_GFPlines+CLAMP(red)/", images,  0)
+plt.hist(clamp2null_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp2 (null)")
+plt.xlabel('Speckle Volume (pixels)')
+plt.ylabel('Probability (number)')
+plt.title(f"Distribution of Speckle Volumes (N={len(clamp2null_volumes)}) (Clamp2 (Null))")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+plt.hist(clamph_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp(H)")
+plt.xlabel('Speckle Volume (pixels)')
+plt.ylabel('Probability (number)')
+plt.title(f"Distribution of Speckle Volumes (N={len(clamph_volumes)}) (Clamp (H))")
+plt.grid(True)
+plt.legend()
+plt.show()
+
+plt.hist(clampi_volumes, bins = 50, edgecolor='k', alpha=0.7, range=(0, 400), density = True, label = "Clamp(I)")
+plt.xlabel('Speckle Volume (pixels)')
+plt.ylabel('Probability (number)')
+plt.title(f"Distribution of Speckle Volumes (N={len(clampi_volumes)}) (Clamp (I))")
+plt.grid(True)
+plt.legend()
+plt.show()
