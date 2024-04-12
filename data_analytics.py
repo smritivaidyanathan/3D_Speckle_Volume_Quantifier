2492647101dfae5e9e8005968c54f6e878e3f377
Smriti Vaidyanathan <smriti_vaidyanathan@brown.edu>
2024-04-12 19:31:58 -0400

    added comments and usage instructions to data analysis

--- data_analytics.py
+++ data_analytics.py
@@ -10,6 +10,34 @@ import csv
 import random
 import math
 
+'''
+Makes distribution histogram with different backgrounds overlayed on top of each other
+
+Parameters:
+data_backgrounds - List of arrays of speckle data, where each outer list represents a background and each inner list represents 
+the pooled values from all movies/z-stacks. Can be created from calling get_data_for_backgrounds on csv_data_dict output from csv_files_to_dict
+
+legend - list of strings representing the names of each experimental background 
+ex: ["Control (CLAMP WT)", "CLAMP delPRLD"]
+
+title - string representing title of graph
+ex: "Distribution of Speckle Volumes"
+
+save_path - string path represnting where we want the figures to be saved. note that this will probably be in the figs folder 
+if you follow the format of my template method at the bottom. Should end in a /
+
+ylim (optional) - float/int - in case we want a ylim, optional parameter, is default = -1 meaning no ylim. 
+
+ranges (optional) - None or range type, if we want to constrict the x limits, then set to = the range value, default is none. 
+
+show (optional) - boolean represnting whether we want to display the plots. default is false (do not show)
+
+bins (optional)- int representing how many bins we want in our histogram (default is 50)
+
+colors (optional)- list of strings representing the colors we want to represent each background. colors are default ['blue', 'deepskyblue', 'red', 'magenta'].
+See https://matplotlib.org/stable/gallery/color/named_colors.html for options
+
+'''
 def make_dist_histogram(data_backgrounds, legend, title, save_path, ylim = -1, ranges = None, show = False, bins=50, colors = ['blue', 'deepskyblue', 'red', 'magenta']):
     for i in range(len(data_backgrounds)):
         data = data_backgrounds[i]
@@ -26,6 +54,25 @@ def make_dist_histogram(data_backgrounds, legend, title, save_path, ylim = -1, r
         plt.show()
     plt.savefig(f'{save_path}{title}')
 
+'''
+Makes basic mean bars representing mean from each background with 95% confidence bars. 
+
+Parameters:
+data_backgrounds - List of arrays of speckle data, where each outer list represents a background and each inner list represents 
+the pooled values from all movies/z-stacks. Can be created from calling get_data_for_backgrounds on csv_data_dict output from csv_files_to_dict
+
+legend - list of strings representing the names of each experimental background 
+ex: ["Control (CLAMP WT)", "CLAMP delPRLD"]
+
+title - string representing title of graph
+ex: "Distribution of Speckle Volumes"
+
+save_path - string path represnting where we want the figures to be saved. note that this will probably be in the figs folder 
+if you follow the format of my template method at the bottom. Should end in a /
+
+show (optional) - boolean represnting whether we want to display the plots. default is false (do not show)
+
+'''
 def make_mean_bar(data_backgrounds, legend, title, save_path,  show = False):
     means = [np.mean(data) for data in data_backgrounds]
     legend_with_N = [f"{legend[i]} (N={len(data_backgrounds[i])})" for i in range(len(legend))]
@@ -40,7 +87,13 @@ def make_mean_bar(data_backgrounds, legend, title, save_path,  show = False):
         plt.show()
     return means, error
 
-def calculate_p_values(data, legend):
+'''
+Calculates pairwise p and t values of all data in data and outputs them into a csv file in /csv/. 
+
+data should be of type list of list of volumes, where outer list is per background, inner list is 
+pooled values from a specific background
+'''
+def calculate_p_and_t_values(data, legend, path_to_csv):
     p_values_table = np.zeros((len(data),len(data)))
     t_values_table = np.zeros((len(data),len(data)))
     for i in range(len(data)):
@@ -60,6 +113,31 @@ def calculate_p_values(data, legend):
         for i, row in enumerate(t_values_table):
             writer.writerow([legend[i]] + list(row))
 
+'''
+Makes mean bar chart with 95% confidence intervals for each background and also plots movie-wise means, movie wise meaning per z-stack. 
+
+Parameters:
+movie_wise_data_backgrounds - List of arrays of arrays of speckle data, where each outer list represents a background and middle lists represent each 
+movie/z-stack, and each inner-most ist represents the values from a movie/z-stack. Can be created from calling get_data_for_background on csv_movie_wise_dict output from csv_files_to_dict
+
+data_backgrounds - List of arrays of speckle data, where each outer list represents a background and each inner list represents 
+the pooled values from all movies/z-stacks. Can be created from csv_data_dict output from calling get_data_for_backgrounds on csv_files_to_dict
+
+legend - list of strings representing the names of each experimental background 
+ex: ["Control (CLAMP WT)", "CLAMP delPRLD"]
+
+title - string representing title of graph
+ex: "Distribution of Speckle Volumes"
+
+save_path - string path represnting where we want the figures to be saved. note that this will probably be in the figs folder 
+if you follow the format of my template method at the bottom. Should end in a /
+
+show (optional) - boolean represnting whether we want to display the plots. default is false (do not show)
+
+colors (optional) - list of strings representing the colors we want to represent each background. colors are default ['blue', 'deepskyblue', 'red', 'magenta'].
+See https://matplotlib.org/stable/gallery/color/named_colors.html for options
+
+'''
 def make_mean_scatter(movie_wise_data_backgrounds, data_backgrounds, legend, title, save_path, show = False,  colors = ['blue', 'deepskyblue', 'red', 'magenta']):
     means = [[np.mean(inner_list) for inner_list in sublist] for sublist in movie_wise_data_backgrounds]
 
@@ -76,6 +154,33 @@ def make_mean_scatter(movie_wise_data_backgrounds, data_backgrounds, legend, tit
     if (show):
         plt.show()
 
+
+'''
+Makes mean or variance bars from each background with 95% confidence bars calculated
+by bootstrapping. 
+
+Parameters:
+data_backgrounds - List of arrays of speckle data, where each outer list represents a background and each inner list represents
+the pooled values from all movies/z-stacks. Can be created from calling get_data_for_backgrounds on csv_data_dict output from csv_files_to_dict
+
+legend - list of strings representing the names of each experimental background 
+ex: ["Control (CLAMP WT)", "CLAMP delPRLD"]
+
+title - string representing title of graph
+ex: "Distribution of Speckle Volumes"
+
+save_path - string path represnting where we want the figures to be saved. note that this will probably be in the figs folder 
+if you follow the format of my template method at the bottom. Should end in a /
+
+show (optional) - boolean represnting whether we want to display the plots. default is false (do not show)
+
+var (optional) - boolean representing whether we want to calculate variance (as opposed to the median). 
+default to calculate the median, ie var = False. 
+
+colors (optional) - list of strings representing the colors we want to represent each background. colors are default ['blue', 'deepskyblue', 'red', 'magenta'].
+See https://matplotlib.org/stable/gallery/color/named_colors.html for options
+
+'''
 def make_med_or_var_bar(data_backgrounds, legend, title, save_path, var = False, show = False, colors = ['blue', 'deepskyblue', 'red', 'magenta']):
     outer_quals = [np.median(data) for data in data_backgrounds]
     ylabel = "Median"
@@ -97,6 +202,9 @@ def make_med_or_var_bar(data_backgrounds, legend, title, save_path, var = False,
         plt.show()
     return outer_quals
 
+'''
+Helper method to construct confidence intervals using bootstrapping. 
+'''
 def bootstrapcli(data , var = False) :
     num_bootstraps = 1000
     bootstrap_quals = []
@@ -113,6 +221,18 @@ def bootstrapcli(data , var = False) :
 
 path_to_exp = "/volumes/Research/BM_LarschanLab/Mukulika/Feb2024/"
 
+
+'''
+Helper method to create necessary dictionary data structures for data analytics.
+
+Creates csv_data_dict, csv_movie_wise_dict, dictionaries of backgrounds mapped to lists of information.
+csv_data_dict - dictionary mapping background to list of pooled data from all z-stacks
+csv_movie_wise_dict - dictionary mapping background to list of lists, where outer lists represent each movie/z-stack
+and each inner list represents the volumes from that movie-z-stack
+
+path_to_csv is a path to the csv folder. should end in /. 
+ex: "/Smriti/Experiments/in-vivo/csv/"
+'''
 def csv_files_to_dict(path_to_csv):
     csv_data_dict = {}
     csv_movie_wise_dict = {}
@@ -132,6 +252,22 @@ def csv_files_to_dict(path_to_csv):
                 csv_movie_wise_dict[background] = data_movie
     return csv_data_dict, csv_movie_wise_dict
 
+'''
+Helper method to create data structure appending data from each selected background from data_backgrounds_dict into
+one list. 
+
+Creates csv_data_dict, csv_movie_wise_dict, dictionaries of backgrounds mapped to lists of information.
+csv_data_dict - dictionary mapping background to list of pooled data from all z-stacks
+csv_movie_wise_dict - dictionary mapping background to list of lists, where outer lists represent each movie/z-stack
+and each inner list represents the volumes from that movie-z-stack
+
+parameters:
+
+data_backgrounds_dict - dictionary mapping background to list of data (can be moviewise or pooled), created by  csv_files_to_dict
+selected_backgrounds - backgrounds from data_backgrounds_dict that we want to include in selected_data
+
+ex: "/Smriti/Experiments/in-vivo/csv/"
+'''
 def get_data_for_backgrounds(data_backgrounds_dict, selected_backgrounds):
     selected_data = []
     for background in selected_backgrounds:
@@ -143,6 +279,29 @@ def get_data_for_backgrounds(data_backgrounds_dict, selected_backgrounds):
     return selected_data
 
 
+'''
+Template method.
+path is path to experiment folder with figs, csv, nd2...
+
+'''
+def sample_data_analysis(path):
+    path_to_csv= path + "csv/"
+    legend = ["Background 1", "Background 2",  "Background 3"]
+    data_backgrounds_dict, csv_movie_wise_dict = csv_files_to_dict(path_to_csv)
+
+    selected_backgrounds = ["back_1", "back_2", "back_3"]
+    data_backgrounds = get_data_for_backgrounds(data_backgrounds_dict, selected_backgrounds)
+    data_backgrounds_movie_wise = get_data_for_backgrounds(csv_movie_wise_dict, selected_backgrounds)
+
+    calculate_p_and_t_values(data_backgrounds, legend, path_to_csv)
+    make_mean_scatter(data_backgrounds_movie_wise, data_backgrounds, legend, "Mean Sample Graph", f'{path}/figs/', show = True)
+    make_dist_histogram(data_backgrounds, legend, "Histogram Sample Graph", f'{path}/figs/', ranges = (0,500), show = True)
+    make_med_or_var_bar(data_backgrounds, legend, "Median Sample Graph",  f'{path}/figs/', show = True)
+    make_med_or_var_bar(data_backgrounds, legend, "Variance Sample Graph",  f'{path}/figs/', var = True, show = True)
+
+'''
+An example method! What I used to create my in vivo figures. 
+'''
 def in_vivo_display_figs_from_exp(path):
     path_to_csv= path + "csv/"
     legend = ["Male WT", "Male delPrLD",  "Female WT", "Female delPrLD"]
@@ -150,12 +309,16 @@ def in_vivo_display_figs_from_exp(path):
     selected_backgrounds = ["Male_Con", "Male_PrLD", "female_con", "Female_PrLD"]
     data_backgrounds = get_data_for_backgrounds(data_backgrounds_dict, selected_backgrounds)
     data_backgrounds_movie_wise = get_data_for_backgrounds(csv_movie_wise_dict, selected_backgrounds)
-    calculate_p_values(data_backgrounds, legend)
+    calculate_p_and_t_values(data_backgrounds, legend, path_to_csv)
     make_mean_scatter(data_backgrounds_movie_wise, data_backgrounds, legend, "Mean of In Vivo Speckle Volumes with Movie-Wise Means", f'{path}/figs/', show = True)
     make_dist_histogram(data_backgrounds, legend, " Distribution of In Vivo Speckle Volumes", f'{path}/figs/', ranges = (0,500), show = True)
     # make_mean_bar(data_backgrounds, selected_backgrounds, "In Vitro Mean of  PrLD speckle Volumes",  f'{path}/figs/', show = True)
     make_med_or_var_bar( data_backgrounds, legend, "Median of In Vivo Speckle Volumes",  f'{path}/figs/', show = True) 
     make_med_or_var_bar( data_backgrounds, legend, "Variance of In Vivo Speckle Volumes",  f'{path}/figs/',  var = True, show = True) 
+
+'''
+an example method! What I used to create my in vitro figures. 
+'''
 
 def in_vitro_display_figs_from_exp(path, colors = ["darkgreen", "lightgreen"]):
     path_to_csv= path + "csv/"
@@ -164,15 +327,23 @@ def in_vitro_display_figs_from_exp(path, colors = ["darkgreen", "lightgreen"]):
     selected_backgrounds = ["CLAMP_WT", "CLAMP_delPRLD"]
     data_backgrounds = get_data_for_backgrounds(data_backgrounds_dict, selected_backgrounds)
     data_backgrounds_movie_wise = get_data_for_backgrounds(csv_movie_wise_dict, selected_backgrounds)
-    calculate_p_values(data_backgrounds, legend)
+    calculate_p_and_t_values(data_backgrounds, legend, path_to_csv)
     make_mean_scatter(data_backgrounds_movie_wise, data_backgrounds, legend, "Mean of In Vitro Speckle Volumes with Movie-Wise Means", f'{path}/figs/', show = True, colors = colors)
     make_dist_histogram(data_backgrounds, legend, " Distribution of In Vitro Speckle Volumes", f'{path}/figs/', ranges = (0,500), show = True, colors = colors)
     # make_mean_bar(data_backgrounds, selected_backgrounds, "In Vitro Mean of  PrLD speckle Volumes",  f'{path}/figs/', show = True)
     make_med_or_var_bar( data_backgrounds, legend, "Median of In Vitro Speckle Volumes",  f'{path}/figs/', show = True, colors = colors) 
     make_med_or_var_bar( data_backgrounds, legend, "Variance of In Vitro Speckle Volumes",  f'{path}/figs/',  var = True, show = True, colors = colors) 
 
-path_to_exp =  "/volumes/Research/BM_LarschanLab/Smriti/in_vitro/"
-in_vitro_display_figs_from_exp(path_to_exp)
-path_to_exp =  "/volumes/Research/BM_LarschanLab/Mukulika/Feb2024/"
-#in_vivo_display_figs_from_exp(path_to_exp)
+'''
+CALL YOUR METHODS BELOW! Make sure to specify the path to your experiment. 
+'''
+
+#Example usage
+# path_to_exp =  "/volumes/Research/BM_LarschanLab/Smriti/in_vitro/"
+# in_vitro_display_figs_from_exp(path_to_exp)
+# path_to_exp =  "/volumes/Research/BM_LarschanLab/Mukulika/Feb2024/"
+# #in_vivo_display_figs_from_exp(path_to_exp)
+
+
+
 
